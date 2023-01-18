009377136b5fdcce06d99ef42e5cf1819c901934
MartinCanolik <martin.canolik@gmail.com>
2023-01-18 18:23:05 -0300

    railway deploy

--- index.js
+++ index.js
@@ -20,9 +20,11 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
+const port = process.env.PORT || 3001;
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-	server.listen(process.env.PORT, () => {
-		console.log("%s listening at 3001"); // eslint-disable-line no-console
+	server.listen(port, () => {
+		console.log(`server listening on port ${port}`); // eslint-disable-line no-console
 	});
 });
